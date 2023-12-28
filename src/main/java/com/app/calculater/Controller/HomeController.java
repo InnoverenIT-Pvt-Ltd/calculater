fadeb0600aad5b73ed54515399880254f4d518eb
PratimaBarik <barik.pratima123@gmail.com>
2023-12-28 15:30:23 +0530

    upload page

--- src/main/java/com/app/calculater/Controller/HomeController.java
+++ src/main/java/com/app/calculater/Controller/HomeController.java
@@ -2,11 +2,18 @@ package com.app.calculater.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
       public String getuploadForm(){
         return "redirect:/upload";
+    }
+    @RequestMapping("/upload")
+    public ModelAndView uploadForm(){
+        ModelAndView mv = new ModelAndView("upload");
+
+        return mv;
     }
 }
