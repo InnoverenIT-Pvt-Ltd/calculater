dda6485063e65e3ea5b6044cba0a45bfbab16982
Ranjeet Sahoo <ranjeetsahoo02@gmail.com>
2023-12-29 18:27:07 +0530

    sc

--- src/main/java/com/app/calculater/Controller/HomeController.java
+++ src/main/java/com/app/calculater/Controller/HomeController.java
@@ -1,7 +1,17 @@
 package com.app.calculater.Controller;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,4 +26,38 @@ public class HomeController {
 
         return mv;
     }
+   
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestParam("name") String name,@RequestParam("age") String age,
+    						@RequestParam("gender") String gender,@RequestParam("file") MultipartFile file,
+    						Model model, HttpServletRequest request) {
+    	System.out.println("name"+ name);
+    	System.out.println("age"+ age);
+    	System.out.println("gender"+ gender);
+    	if (file.isEmpty()) {
+            model.addAttribute("message", "Please select a file to upload");
+            return "uploadResult"; // Points to the uploadResult.jsp file
+        }
+
+        try {
+            ServletContext servletContext = request.getServletContext();
+            String uploadsDir = "/uploads/"; // Directory to save uploaded files
+            String realPath = servletContext.getRealPath(uploadsDir);
+
+            if (!new File(realPath).exists()) {
+                new File(realPath).mkdir();
+            }
+
+            String originalFileName = file.getOriginalFilename();
+            String filePath = realPath + originalFileName;
+            File dest = new File(filePath);
+            file.transferTo(dest);
+
+            model.addAttribute("message", "File uploaded successfully");
+        } catch (IOException e) {
+            model.addAttribute("message", "File upload failed: " + e.getMessage());
+        }
+        return "uploadResult"; // Points to the uploadResult.jsp file
+    }
+    
 }
--- src/main/webapp/WEB-INF/jsp/upload.jsp
+++ src/main/webapp/WEB-INF/jsp/upload.jsp
@@ -1,3 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
 <html>
-hello
+<head>
+    <title>File Upload Example</title>
+</head>
+<body>
+    <h2>Upload File</h2>
+    <form method="POST" action="/uploadFile" enctype="multipart/form-data">
+        <input type="file" name="file" /><br/><br/>
+        <h3>Name</h3>
+        <input type="text" name="name" />
+        <h3>Age</h3>
+        <input type="number" name="age" id="age" min="0" required/><br/><br/>
+        <label for="gender">Gender:</label>
+        <select name="gender">
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="other">Other</option>
+        </select></br></br>
+        <input type="submit" value="Submit" />
+    </form>
+</body>
 </html>
